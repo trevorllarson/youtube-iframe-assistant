19613465b79b62723606fb131f35eacdcfce4f1f
Trevor Larson <trevor@electricpulp.com>
2017-10-06 16:16:46 -0500

    Updates

--- youtube-iframe-assistant.js
+++ youtube-iframe-assistant.js
@@ -6,6 +6,56 @@
 
     };
 
-    
+
+    // TODOs
+    // On close
+    $('.modal.modal-video').on('hidden.bs.modal', function () {
+        var iframes = document.getElementsByTagName("iframe");
+        if (iframes !== null) {
+            for (var i = 0; i < iframes.length; i++) {
+                var newSrc = iframes[i].src;
+                newSrc = newSrc.replace('&autoplay=1', '');
+                iframes[i].src = newSrc; //causes a reload so it stops playing, music, video, etc.
+            }
+        }
+    });
+
+    // Aspect Ratio
+    var $allVideos = $("iframe");
+
+    $allVideos.each(function() {
+        // console.log('Video -------');
+        // console.log($(this).height());
+        $(this).attr('data-aspectRatio', $(this).height() / $(this).width()).removeAttr('height').removeAttr('width');
+        $(this).attr('data-aspectRatio', .5625);
+        $(this).addClass('video-hiding');
+    });
+
+    $(window).resize(function() {
+        $allVideos.each(function() {
+            var $el = $(this);
+            $el.css('width', '').css('height', '');
+            $el
+                .width($el.parent('figure').width())
+                .height($el.parent('figure').width() * $el.attr('data-aspectRatio'));
+        });
+    }).resize();
+
+
+    // On open
+    $('.image-container-video').on('click', function () {
+        var that = $(this);
+        var target = that.data('target');
+        var iframe = $(target).find('iframe');
+
+        iframe[0].src += "&autoplay=1";
+
+        setTimeout(function () {
+            iframe.width(iframe.parent('figure').width())
+                .height(iframe.parent('figure').width() * iframe.attr('data-aspectRatio'));
+
+            iframe.removeClass('video-hiding');
+        }, 500);
+    });
 
 }( jQuery ));
