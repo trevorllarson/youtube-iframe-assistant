787de77c2c66860e8f1b98d60eebb98d1b6e53b1
Trevor Larson <trevor@electricpulp.com>
2017-10-06 14:06:40 -0500

    Initial Commit

---
+++ youtube-iframe-assistant.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+    $.fn.lockVideoRatio = function(elements) {
+
+        console.log('Lock Video Ratio');
+
+    };
+
+    $.fn.objectVisualLock = function(offset) {
+
+        
+
+    };
+
+}( jQuery ));
