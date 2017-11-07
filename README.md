64041f2a3222b8c58e9ad143fed5ab8dc7a341a5
Trevor Larson <trevor@electricpulp.com>
2017-11-06 21:26:28 -0600

    Updates Readme and adjusts to new build out in WP Base Build

--- README.md
+++ README.md
@@ -1,5 +1,76 @@
 # YouTube Iframe Assistant
 
-### Assistant package for embeded YouTube Iframe Videos
+Make all Iframe YouTube videos fully responsive and scale with the screen
+
+### If using webpack
 
 `var youtubeIframeAssistant = require('youtube-iframe-assistant');`
+
+### Getting Started
+
+Simply call the `youtubeIframeAssistant` function and all iframes will be automatically set to scale responsively
+
+```
+$(function () {
+ 
+    youtubeIframeAssistant();
+ 
+});
+```
+
+You can pass in different settings to tweak your criterea and functionality
+
+```
+$(function () {
+ 
+    youtubeIframeAssistant({
+        useBootstrapModal: true
+    });
+ 
+});
+```
+
+You can either surround your iframe with a figure tag, or allow the plugin to automatically wrap the iframe if it is not already the parent element.
+
+```
+<iframe width="560" height="315" src="https://www.youtube.com/embed/RQ2BHsL4C0I?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+ 
+---- OR ----
+ 
+<figure>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/RQ2BHsL4C0I?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</figure>
+```
+
+### Bootstrap Modals
+
+This plugin can be used along side Bootstrap's Modals with no adjustments, and default styles can be toggled on/off. 
+
+```
+<div class="btn btn-primary video-toggle" data-toggle="modal" data-target="#target-modal">Open Video</div>
+ 
+<div id="target-modal" class="modal fade video-modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+                <iframe width="560" height="315" src="http://youtube.videosrc" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+```
+
+### Settings
+
+| Setting | Type | Default | Description |
+| ------- | ---- | ------- | ----------- |
+| resizeWithWindow | boolean | true | Whether or not the iframe will be resized when the window changes size |
+| useBootstrapModal | boolean | false | Set this to true if using Bootstraps modals as video popups |
+| startVideoOnModalOpen | boolean | true | If using Bootstrap Modals, start the video when the modal opens, based on videoTogglesClass |
+| stopVideoOnModalClose | boolean | true | If using Bootstrap Modals, stop the video when the modal closes, based on the videoModalsClass |
+| useIframeClass | boolean | false | Change the scope of the iframes effected down to a class |
+| iframeClass | string | ".iframe-video" | Class of the iframes to be scoped to |
+| videoTogglesClass | string | ".video-toggle" | The class that will be watched in order to trigger the video play |
+| videoModalsClass | string | ".video-modal" | The class that will be used to watch for modal closes to stop the video contained |
--- youtube-iframe-assistant.js
+++ youtube-iframe-assistant.js
@@ -1,117 +1,98 @@
 (function ($) {
 
-    $.fn.youtubeIframeAssistant = function(options) {
+    youtubeIframeAssistant = function(options) {
 
         var settings = $.extend({
+            resizeWithWindow: true,
+            useBootstrapModal: false,
+            startVideoOnModalOpen: true,
+            stopVideoOnModalClose: true,
+            useIframeClass: false,
             videoTogglesClass: ".video-toggle",
             videoModalsClass: ".video-modal",
-            iframeClass: ".iframe-video",
-            setAspectRatio: true
+            iframeClass: ".iframe-video"
         }, options );
 
-        // Stops the video when the modal closes
-        if($(settings.videoModalsClass).length) {
-            $('.modal' . settings.videoModalsClass).on('hidden.bs.modal', function () {
-                var iframes = $(this).find('iframe'),
-                    iframe = iframes[0];
+        // Get all video iframe objects
+        var allVideos = (settings.useIframeClass) ? $('iframe' + settings.iframeClass) : $('iframe');
 
-                stopIframe(iframe);
+        if(!allVideos.length) return;
+
+        // Check if utilizing Bootstrap modals as popups
+        if(settings.useBootstrapModal) {
+
+            // Stops the video when the modal closes
+            if(settings.stopVideoOnModalClose && $('.modal' + settings.videoModalsClass).length) {
+                $('.modal' + settings.videoModalsClass).on('hidden.bs.modal', function () {
+                    var iframes = $(this).find('iframe'),
+                        iframe = iframes[0];
+
+                    stopIframe(iframe);
+                });
+            }
+
+        }
+
+        allVideos.each(function () {
+            var video = $(this),
+                videoParent = $(this).parent(),
+                src = video.attr('src'),
+                aspectRatio = video.height() / video.width();
+
+            if(!videoParent.is('figure')) {
+                video.wrap('<figure></figure>');
+            }
+
+            // Failsafe for attaching the autoplay attribute later on. If there isn't already a variable the video will break.
+            if(src.indexOf("?") < 0) {
+                video.attr('src', src + '?rel=0');
+            }
+
+            video.attr('data-aspect-ratio', aspectRatio);
+            video.removeAttr('height').removeAttr('width');
+
+            sizeVideo(video);
+        });
+
+
+        if(settings.resizeWithWindow) {
+            $(window).resize(function() {
+                allVideos.each(function() {
+                    sizeVideo($(this));
+                });
             });
         }
 
-        if(settings.setAspectRatio) {
-            console.log('Set Aspect Ratio - True');
-            setIframeAspectRatio($(settings.iframeClass));
-        } else {
-            // Remove Else After Testing
-            console.log('Set Aspect Ratio - False');
+
+        if(settings.startVideoOnModalOpen) {
+            $(settings.videoTogglesClass).on('click', function () {
+                var target = $(this).data('target');
+                var iframes = $(target).find('iframe'),
+                    iframe = iframes[0];
+
+                startIframe(iframe);
+            });
         }
 
     };
+
+    function sizeVideo(video) {
+        video.css({ width: '', height: '' });
+
+        video.css({
+            width: video.parent('figure').actual('width'),
+            height: video.parent('figure').actual('width') * video.attr('data-aspect-ratio')
+        });
+    }
+
+    function startIframe(iframe) {
+        iframe.src += "&autoplay=1";
+    }
 
     function stopIframe(iframe) {
         var newSrc = iframe.src;
         newSrc = newSrc.replace('&autoplay=1', '');
         iframe.src = newSrc;
     }
-
-    function setIframeAspectRatio(iframes) {
-
-        if(!iframes.length) {
-            iframes = ".iframe-video";
-        }
-
-        var allVideos = $(iframes);
-
-        allVideos.each(function () {
-
-            var aspectRatio = $(this).height() / $(this).width();
-            console.log(aspectRatio);
-
-        });
-
-
-        $allVideos.each(function() {
-            // console.log('Video -------');
-            // console.log($(this).height());
-            $(this).attr('data-aspectRatio', $(this).height() / $(this).width()).removeAttr('height').removeAttr('width');
-            $(this).attr('data-aspectRatio', .5625);
-            $(this).addClass('video-hiding');
-        });
-
-        $(window).resize(function() {
-            $allVideos.each(function() {
-                var $el = $(this);
-                $el.css('width', '').css('height', '');
-                $el
-                    .width($el.parent('figure').width())
-                    .height($el.parent('figure').width() * $el.attr('data-aspectRatio'));
-            });
-        }).resize();
-    }
-
-    $.fn.lockVideoRatio = function(elements) {
-
-        console.log('Lock Video Ratio');
-
-    };
-
-    // Aspect Ratio
-    var $allVideos = $("iframe");
-
-    $allVideos.each(function() {
-        // console.log('Video -------');
-        // console.log($(this).height());
-        $(this).attr('data-aspectRatio', $(this).height() / $(this).width()).removeAttr('height').removeAttr('width');
-        $(this).attr('data-aspectRatio', .5625);
-        $(this).addClass('video-hiding');
-    });
-
-    $(window).resize(function() {
-        $allVideos.each(function() {
-            var $el = $(this);
-            $el.css('width', '').css('height', '');
-            $el
-                .width($el.parent('figure').width())
-                .height($el.parent('figure').width() * $el.attr('data-aspectRatio'));
-        });
-    }).resize();
-
-
-    // On open
-    $('.image-container-video').on('click', function () {
-        var that = $(this);
-        var target = that.data('target');
-        var iframe = $(target).find('iframe');
-
-        iframe[0].src += "&autoplay=1";
-
-        setTimeout(function () {
-            iframe.width(iframe.parent('figure').width())
-                .height(iframe.parent('figure').width() * iframe.attr('data-aspectRatio'));
-
-            iframe.removeClass('video-hiding');
-        }, 500);
-    });
 
 }( jQuery ));
